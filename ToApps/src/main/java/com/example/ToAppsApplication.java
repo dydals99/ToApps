cac9226b82b3b8ef43c7bf1aa32fe739e1d977fb
dydals99 <whdydals8604@gmail.com>
2024-06-10 15:18:21 +0900

    war 배포 - build.gradle 수정

--- ToApps/src/main/java/com/example/ToAppsApplication.java
+++ ToApps/src/main/java/com/example/ToAppsApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToAppsApplication {
-
+public class ToAppsApplication  {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ToAppsApplication.class, args);
 	}
