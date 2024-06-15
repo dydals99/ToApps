022572d8eccda2ed8264ec8ee0a7501c2f856d31
dydals99 <whdydals8604@gmail.com>
2024-06-16 01:08:30 +0900

    fin

--- ToApps/src/main/java/com/example/ToAppsApplication.java
+++ ToApps/src/main/java/com/example/ToAppsApplication.java
@@ -2,12 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToAppsApplication  {
+public class ToAppsApplication  extends SpringBootServletInitializer{
 	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ToAppsApplication.class, args);
 	}
